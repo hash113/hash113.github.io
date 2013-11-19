6141adce55927f825e1059e99773781033bd5e81
Hash113 <harshbhatia296@gmail.com>
2013-11-20 02:57:04 +0530

    mark1 begin

---
+++ Scripts/script.js
@@ -0,0 +1,12 @@
+
+$( document ).ready(function() {
+	$('#rightsplit').css('clip','rect(0px,1400px,800px,650px)')
+	$('#leftsplit').css('clip','rect(0px,650px,800px,0px)')
+});
+
+
+/*
+$( window ).load(function() {
+    console.log( "window loaded" );
+});
+*/
