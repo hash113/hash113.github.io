9a5e4fa9bec0f3c682e808ebe809d5856af77074
Hash113 <harshbhatia296@gmail.com>
2013-11-26 00:25:19 +0530

    linking and seperation done

--- js/script.js
+++ js/script.js
@@ -12,15 +12,27 @@ $( window ).load(function()
     
     $('#white_half').hover(function()
 	    {
-	 	   $("#white_half").css("background-color","black");
+	 	   // $("#white_half").css("background-color","black");
 	  		},function(){
 	  		$("#white_half").css("background-color","#ececec");
 		});
 
      $('#black_half').hover(function()
 	    {
-	 	   $("#black_half").css("background-color","#ececec");
+	 	   // $("#black_half").css("background-color","#ececec");
+	 	   $('#matrix').css("opacity","0.4");
 	  		},function(){
 	  		$("#black_half").css("background-color","black");
+	  		$('#matrix').css("opacity","0.2");
 		});
+
+     $('#white_half').click(function()
+     {
+     	window.open("creativity.html","_self");
+     });
+
+     $('#black_half').click(function()
+     {
+     	window.open("programming.html","_self");
+     });
 });
