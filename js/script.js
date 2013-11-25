79699bc73048a08a4d912990f506f0f991d6cf84
Hash113 <harshbhatia296@gmail.com>
2013-11-25 12:58:03 +0530

    today's job done

--- js/script.js
+++ js/script.js
@@ -2,9 +2,25 @@
 $( document ).ready(function() {
 	});
 
+// window.onload=function(){
 
-/*
-$( window ).load(function() {
+// }
+
+$( window ).load(function() 
+{
     console.log( "window loaded" );
+    
+    $('#white_half').hover(function()
+	    {
+	 	   $("#white_half").css("background-color","black");
+	  		},function(){
+	  		$("#white_half").css("background-color","#ececec");
+		});
+
+     $('#black_half').hover(function()
+	    {
+	 	   $("#black_half").css("background-color","#ececec");
+	  		},function(){
+	  		$("#black_half").css("background-color","black");
+		});
 });
-*/
