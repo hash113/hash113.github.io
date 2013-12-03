13b3aaa3e38e78a57b474b6dab02f9cfa347ef12
Hash113 <harshbhatia296@gmail.com>
2013-12-04 01:32:58 +0530

    creativity side changed

--- js/script_c.js
+++ js/script_c.js
@@ -2,12 +2,15 @@ $( window ).load(function()
 {
  	$("#one,.header").fadeIn("slow");
 
-	$('#sketches').click(function(){
-		$("#one").fadeOut("slow");
+ 	$(".sketch").hover(function(){
+ 		$("#hide").fadeIn("slow")},function(){
+ 			$("#hide").fadeOut("slow")
+ 	});
+
+
+// onclick link
+	$('.sketch').click(function(){
 		window.open("sketches.html","_self");
 	});
 
-	$('#graphics').click(function(){
-		window.open("hhh.html","_self");
-	})
 });
