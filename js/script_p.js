d0169ad864bca25b0b3b37f83392d5bee8ccc39d
Hash113 <harshbhatia296@gmail.com>
2014-01-04 02:11:20 +0530

    projects section

--- js/script_p.js
+++ js/script_p.js
@@ -13,13 +13,36 @@ function loader(){
  		$("#sketch_cover").fadeIn("slow")},function(){
  			$("#sketch_cover").fadeOut("slow")
  	});
+ 	$(".back").click(function(){
+ 		$(this).fadeOut(0);
+ 		$(".projectdesc").fadeOut(0);
+ 		$(".playground,.blog,.playground").fadeIn(0);
+ 		$(".centerfont").animate({
+			paddingTop:"30%"
+			},
+			500);
+		$( ".project,.blog,.playground" ).animate({
+		    width: "32%",
+		    marginLeft: "10px"
+		  }, 500 );
 
+		
+ 	});
 
 // onclick link
 	$('.project').click(function(){
+		$(".back,.projectdesc").fadeIn(0);
+		$(".playground,.blog").fadeOut(0);
 		
-		// $("body").slideUp(600);
-		window.open("https://github.com/hash113?tab=repositories","_self");
+
+		$(".centerfont").animate({
+			paddingTop:"1%"
+			},
+			500);
+		$( ".project" ).animate({
+		    width: "90%",
+		    marginLeft: "5%"
+		  }, 500 );
 
 	});
 }
