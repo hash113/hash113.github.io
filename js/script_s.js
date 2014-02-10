7aebda30d51da6116e7ec1ca551f79b73b00067b
Hash113 <harshbhatia296@gmail.com>
2014-02-10 11:34:41 +0530

    header twicling

--- js/script_s.js
+++ js/script_s.js
@@ -6,5 +6,21 @@ $( window ).load(function()
 function loader(){
 	$("#load").fadeOut("slow");
 	$("#container").fadeIn("slow");
-	$('.carousel').carousel();
+	// $('.carousel').carousel();
+	
+	// if $('img:nth-child(2)').position().top);
+
+// 	$(window).scroll(function() {
+
+// var winTop = $(this).scrollTop();
+// var $imgs = $('img:nth-child(2)');
+
+// $.each($imgs, function(item) {
+//     if($(item).position().top = winTop)
+//     {
+//     	alert("top");
+//     }
+         //get id here
+
+// });
 }
