4a61c8cf846c42bf7541db5b5a4438b123e1077d
Hash113 <harshbhatia296@gmail.com>
2014-02-13 23:33:16 +0530

    hover effect addition

--- js/script.js
+++ js/script.js
@@ -1,53 +1,27 @@
 
-$( document ).ready(function() {
-	//console.log("first");
-	});
-	// $(window).load(function() { callFunction(); });
-// window.onload=function(){
-
-// }
-
 $( window ).load(function() 
 {
-   	// console.log( "window loaded" );
-    $("#spinner").css("display","block");
+   	
+    // $("#spinner").css("display","block");
     loader();
 
 });
 
 function loader()
 {
-	
 // display images
-    $("#spinner").css("display","none");
-	$('.none,#link_main').delay(50).fadeIn();
-    $('#logosvg').animate({marginTop:0},"slow");
-    
-    $('#art-button').hover(function(){
-    	$(".artdisplay").fadeIn('slow');
-    	$(".prodisplay").fadeOut('slow');
-    	},
-    	function(){
-	$(".artdisplay").fadeOut('slow');
-    	}
-    )
-$('#programming-button').hover(function(){
-    	$(".prodisplay").fadeIn('slow');},
-    	function(){
-	$(".prodisplay").fadeOut('slow');
-    	
-    })
+    // $("#spinner").css("display","none");
+    $('#art').hover(function(){
+                // document.body.style.backgroundImage=url('../images/wallpaper_art.jpg');
+                $(".fullimage").fadeIn(700);
+                $(".display").text("Art ");
+                $(".name").css('color', 'red');
 
-// click functions
-	
-$('#art-button').click(function(){
-		window.open("creativity.html","_self");
-    });
-$('#programming-button').click(function(){
-		window.open("programming.html","_self");
-    });
-	$('#link_main').click(function(){
-    	$('#welcometab').fadeOut("slow");
-    	$('.foot,.boxlink').fadeIn("slow");
-    });
+    	},
+        function(){
+            $(".fullimage").fadeOut(500);
+            $(".display").text("H B");
+            $(".name").css('color', 'black');
+        }
+        );
 }
