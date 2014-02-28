ba15b6928cf61a6867f1fa502febef528d509b1b
Hash113 <harshbhatia296@gmail.com>
2014-02-28 23:32:36 +0530

    several bugs fixed

--- js/script.js
+++ js/script.js
@@ -1,65 +1,61 @@
+$(window).load(function() {
 
-$( window ).load(function() 
-{
-   	
     // $("#spinner").css("display","block");
     loader();
 
 });
 
-function loader()
-{
-// display images
+function loader() {
+    // display images
     // $("#spinner").css("display","none");
-    $('#art').hover(function(){
-                $(".fullimage").attr("src","images/wallpaper_art2.png");
-                $(".fullimage").fadeIn(700);
-                $(".display").text("Art ");
-                $(".name").css('color', 'red');
-    	},
-        function(){
-            $(".fullimage").fadeOut(500);
-            $(".display").text("H B");
-            $(".name").css('color', 'black');
-        }  );
-
-    $('#programming').hover(function(){
-                $(".fullimage").attr("src","images/wallpaper_mat.png");
-                $(".fullimage").fadeIn(700);
-                $(".display").text("Projects ");
-                $(".name").css('color', 'green');
+    $('#art').hover(function() {
+            $(".fullimage").attr("src", "images/wallpaper_art2.png");
+            $(".fullimage").fadeIn(700);
+            $(".display").text("Art ");
+            $(".name").css('color', 'red');
         },
-        function(){
+        function() {
             $(".fullimage").fadeOut(500);
-            $(".display").text("H B");
+            $(".display").text("H.B.");
             $(".name").css('color', 'black');
-            $(".fullimage").attr("src","images/wallpaper_art2.png");
-        }  );
+        });
 
-    $('#blog').hover(function(){
-                $(".fullimage").attr("src","images/wallpaper_blog.png");
-                $(".fullimage").fadeIn(700);
-                $(".display").text("Blog ");
-                $(".name").css('color', 'white');
+    $('#programming').hover(function() {
+            $(".fullimage").attr("src", "images/wallpaper_mat.png");
+            $(".fullimage").fadeIn(700);
+            $(".display").text("Projects ");
+            $(".name").css('color', 'green');
         },
-        function(){
+        function() {
             $(".fullimage").fadeOut(500);
-            $(".display").text("H B");
+            $(".display").text("H.B.");
             $(".name").css('color', 'black');
-        }  );
+            // $(".fullimage").attr("src","images/wallpaper_art2.png");
+        });
 
-    $('#about').hover(function(){
-        $(".fullimage").attr("src","images/wallpaper_ab.png");
-                $("span,.artdesc,.fullimage").fadeIn(700);
-                $(".display").text("About");
-                $(".name").css('color', 'blue');
+    $('#blog').hover(function() {
+            $(".fullimage").attr("src", "images/wallpaper_blog.png");
+            $(".fullimage").fadeIn(700);
+            $(".display").text("Blog ");
         },
-        function(){
+        function() {
+            $(".fullimage").fadeOut(500);
+            $(".display").text("H.B.");
+            $(".name").css('color', 'black');
+        });
+
+    $('#about').hover(function() {
+            $(".fullimage").attr("src", "images/wallpaper_ab.png");
+            $("span,.artdesc,.fullimage").fadeIn(700);
+            $(".display").text("About");
+            $(".name").css('color', 'blue');
+        },
+        function() {
 
             $("span,.artdesc,.fullimage").fadeOut(500);
-            $(".display").text("H B");
+            $(".display").text("H.B.");
             $(".name").css('color', 'black');
-        }  );
+        });
 
 
 }
